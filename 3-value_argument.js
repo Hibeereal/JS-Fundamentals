c4fb0db639c0d072d6c56333965c3c3cfe778660
akinola ibukun <akinolaibukun182@gmail.com>
2025-07-13 23:12:59 +0100

    argument

--- 3-value_argument.js
+++ 3-value_argument.js
@@ -1,7 +1,9 @@
 let arg = process.argv.slice(2);
+let user = arg.join(" ")
+let firstWord = user.split(" ")[0];
 
-if (arg.length === 0){
+if (!firstWord){
   console.log("No argument")
 }else{
-  console.log(arg[0]);
+  console.log(firstWord);
 }
