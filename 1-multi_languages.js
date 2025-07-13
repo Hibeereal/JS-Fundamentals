c372a8e9c7d0430161d60408036208fffbb369e9
akinola ibukun <akinolaibukun182@gmail.com>
2025-07-13 11:47:55 +0100

    spelling correction

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,4 +1,4 @@
-let task = ["C is fun", "python is cool", "javascript is amazing"];
+let task = ["C is fun", "Python is cool", "javascript is amazing"];
 
 for(let i = 0; i < task.length; i++){
   console.log(task[i]);
