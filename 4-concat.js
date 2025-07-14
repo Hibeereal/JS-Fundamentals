862ae0e8384d2daeb9a9046364f48bc8252965bc
akinola ibukun <akinolaibukun182@gmail.com>
2025-07-14 21:38:46 +0100

    concat

--- 4-concat.js
+++ 4-concat.js
@@ -1,7 +1,3 @@
 let arg = process.argv.slice(2);
 
-if (arg.length === 2){
-  console.log(`${arg[0]} is ${arg[1]}`);
-}else if(arg.length !== 2){
-  console.log(`${undefined} is ${undefined}`);
-}
+console.log(`${arg[0]} is ${arg[1]}`);
